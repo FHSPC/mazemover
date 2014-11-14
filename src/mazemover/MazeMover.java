b2f8a54a0bba0e803bd6c27eee8711372ec9b205
alecastle <amueller474@gmail.com>
2014-11-14 09:38:19 -0600

    Create MazeMover.java

---
+++ src/mazemover/MazeMover.java
@@ -0,0 +1,12 @@
+package mazemover;
+/**
+ * this is merely a meek gateway to the rest of our filepath
+ * @author alecastle
+ */
+public class MazeMover
+{
+    public static void main(String[] args)
+    {
+        //
+    }
+}
