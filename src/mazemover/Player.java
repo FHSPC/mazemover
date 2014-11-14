81d154c9abff1ed102a8fa7c1afe6962b1fe2833
alecastle <amueller474@gmail.com>
2014-11-14 09:41:45 -0600

    Create Player.java

---
+++ src/mazemover/Player.java
@@ -0,0 +1,25 @@
+package mazemover;
+import javax.swing.ImageIcon;
+/**
+ * this class represents a Player object
+ * @author alecastle
+ */
+public class Player
+{
+    /**
+     * image used for the player on the board
+     */
+    public ImageIcon display;
+    /**
+     * whether it is the Player's turn
+     */
+    public boolean turn;
+    /**
+     * the Player's score
+     */
+    public int score = 0;
+    public Player(String file)
+    {
+        //
+    }
+}
